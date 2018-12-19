8a040bddb81002a0d2d106d9bb5ec572161fef5c
williamjzhao <wzhao2017@gmail.com>
2018-12-19 17:59:29 -0600

    adding hashHistroy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Router, Route, Link, IndexRoute } from 'react-router';
+import hashHistory from 'react-router-dom';
 import Website from './website'
 import loginTest from './login'
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router history={hashHistory}>
         <Route path='/' component={Website} />
         <Route path='/login' component={loginTest} />
       </Router>
