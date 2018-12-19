4a34e744454b3960f1a1bd7d6acefe41ae7eb024
williamjzhao <wzhao2017@gmail.com>
2018-12-19 17:55:13 -0600

    debuggin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
+import { Router, Route, Link, IndexRoute } from 'react-router';
 import Website from './website'
 import loginTest from './login'
 
 class App extends Component {
   render() {
     return (
-      <Router history={hashHistory}>
+      <Router>
         <Route path='/' component={Website} />
         <Route path='/login' component={loginTest} />
       </Router>
