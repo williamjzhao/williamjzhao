c72d244f35b32a852330865c7a8dcd20697f69af
williamjzhao <wzhao2017@gmail.com>
2018-12-19 18:12:00 -0600

    more routing stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
-import { Router, Route, Link, IndexRoute } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Website from './website'
 import loginTest from './login'
 
 class App extends Component {
   render() {
     return (
-    <BrowserRouter>
-        <div>
-            <Route path='/' component={Website} />
-            <Route path='/login' component={loginTest} />
-        </div>
-    </BrowserRouter>
+        <main>
+            <Switch>
+                <Route exact path='/' component={Website} />
+                <Route path='/login' component={loginTest} />
+            </Switch>
+        </main>
     )
   }
 }
