ed45d81d2bd57f777c910884eff9c75a10cb95df
williamjzhao <wzhao2017@gmail.com>
2018-12-21 17:46:18 -0600

    added a fetch

--- src/login.js
+++ src/login.js
@@ -18,6 +18,11 @@ class Login extends Component {
 
     onSignIn = (googleUser) => {
         let profile = googleUser.getBasicProfile();
+        let id_token = googleUser.getAuthResponse().id_token;
+        fetch('http://localhost:3001/auth', {
+            method: "POST",
+            body: id_token
+        }).then(response => response.json()).then(ok => console.log(ok));
         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
