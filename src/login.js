f5d5a669449ef637a5c41e421595ffbf89d0cc45
williamjzhao <wzhao2017@gmail.com>
2018-12-21 21:37:11 -0600

    hiding login stuff

--- src/login.js
+++ src/login.js
@@ -31,7 +31,7 @@ class Login extends Component {
     onSignIn = (googleUser) => {
         let profile = googleUser.getBasicProfile();
         let id_token = googleUser.getAuthResponse().id_token;
-        await this.authenicate(id_token);
+        let body = await this.authenicate(id_token);
         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
