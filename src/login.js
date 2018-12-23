f18c33b02b012cc28adac578c86e6455db6d136f
williamjzhao <wzhao2017@gmail.com>
2018-12-23 14:15:08 -0600

    working login that checks for existing user

--- src/login.js
+++ src/login.js
@@ -89,11 +89,6 @@ class Login extends Component {
             }
             const add = this.addUser(JSON.stringify(user));
         }
-
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     }
     render() {
         return (
